beedc3472a7496982dfe94beca377f9bfa32784e
JelyFishhhhhh_ _ <tony0940419@gmail.com>
2024-07-01 02:53:54 +0800

    Commit

---
+++ signup/signup.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCoagWFf-LBWsTuosgoiv5QXrYH5xeU_O4",
+    authDomain: "ic-im-in-cse-2024-3ff06.firebaseapp.com",
+    projectId: "ic-im-in-cse-2024-3ff06",
+    storageBucket: "ic-im-in-cse-2024-3ff06.appspot.com",
+    messagingSenderId: "634291546192",
+    appId: "1:634291546192:web:94f81bf7ea5f175ff00955"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
