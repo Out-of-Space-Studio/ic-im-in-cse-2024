6066164debd0d1824f385961104200ab520c18e6
JelyFishhhhhh_ _ <tony0940419@gmail.com>
2024-07-01 21:59:21 +0800

    try 2 make it more beautiful

--- menu-fixed.js
+++ menu-fixed.js
@@ -1,6 +1,27 @@
 // To close the navbar stuff
-function menu_close() {
-    var menu_btn = document.getElementById("menu-btn");
-    menu_btn.checked = false;
-    console.log("Success!");
-}
+document.addEventListener('click', function(event) {
+    var isClickInside = document.querySelector('.header').contains(event.target);
+    var menuBtn = document.getElementById('menu-btn');
+    
+    if (!isClickInside && menuBtn.checked) {
+        menuBtn.checked = false;
+    }
+});
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth",
+        });
+    });
+});
+
+window.addEventListener('scroll', function() {
+    var header = document.querySelector('.header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
