ff5b6392f78c87dac66c509fa88ef9f0b5dae277
JelyFishhhhhh_ _ <tony0940419@gmail.com>
2024-07-02 16:55:24 +0800

    frontend

--- signup/signup.js
+++ signup/signup.js
@@ -65,25 +65,25 @@ function closeImage(event) {
     }
 }
 
-function check_form(){
+function check_form() {}
 
-
-}
+// db stuff
 
 // Import the functions you need from the SDKs you need
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCoagWFf-LBWsTuosgoiv5QXrYH5xeU_O4",
-    authDomain: "ic-im-in-cse-2024-3ff06.firebaseapp.com",
-    projectId: "ic-im-in-cse-2024-3ff06",
-    storageBucket: "ic-im-in-cse-2024-3ff06.appspot.com",
-    messagingSenderId: "634291546192",
-    appId: "1:634291546192:web:94f81bf7ea5f175ff00955",
+    apiKey: "AIzaSyCPh9Hg4qFMyrP_7nchVGTvM-qQKSUa6JE",
+    authDomain: "ic-im-in-cse-2024-48f2b.firebaseapp.com",
+    projectId: "ic-im-in-cse-2024-48f2b",
+    storageBucket: "ic-im-in-cse-2024-48f2b.appspot.com",
+    messagingSenderId: "259899984268",
+    appId: "1:259899984268:web:888b208395c19ffdd3aea9",
 };
 
 // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
