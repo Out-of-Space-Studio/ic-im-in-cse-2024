dd83a55e249018cf4e9017db2ee575b46d9d32ee
JelyFishhhhhh_ _ <tony0940419@gmail.com>
2024-07-02 15:06:51 +0800

    form style alpha

--- signup/signup.js
+++ signup/signup.js
@@ -11,6 +11,12 @@ function closeImage(event) {
         modal.style.display = "none";
     }
 }
+
+function check_form(){
+
+    
+}
+
 // Import the functions you need from the SDKs you need
 // import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 // TODO: Add SDKs for Firebase products that you want to use
