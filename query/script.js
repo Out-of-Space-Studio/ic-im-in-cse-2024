712579d1c356dbc189eac725b692ba2e93c2045f
JelyFishhhhhh_ _ <tony0940419@gmail.com>
2024-07-17 02:55:33 +0800

    QUERY ID verify

--- query/script.js
+++ query/script.js
@@ -113,6 +113,21 @@ window.query = async function () {
         return;
     }
 
+    if (!verifyId(form.IDNumber.value)) {
+        alert(`身份證字號錯誤!`);
+        setTimeout(() => {
+            IDNumber.style.borderColor = "red";
+            IDNumber.scrollIntoView({
+                behavior: "smooth",
+                block: "center",
+            });
+            IDNumber.focus();
+        }, 0);
+        return;
+    } else {
+        IDNumber.style.borderColor = "";
+    }
+
     const student = new Stu({
         sid: form.SID.value,
         idnumber: form.IDNumber.value,
@@ -240,3 +255,27 @@ async function readUserData(stu) {
             alert("伺服器發生錯誤，請稍後再試\n錯誤訊息: " + error.message);
         });
 }
+
+// ID Verfication
+function verifyId(id) {
+    id = id.trim();
+    const verification = id.match("^[A-Z][12]\\d{8}$");
+    if (!verification) {
+        return false;
+    }
+
+    let conver = "ABCDEFGHJKLMNPQRSTUVXYWZIO";
+    let weights = [1, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1];
+
+    id = String(conver.indexOf(id[0]) + 10) + id.slice(1);
+
+    let checkSum = 0;
+    for (let i = 0; i < id.length; i++) {
+        const c = parseInt(id[i]);
+        const w = weights[i];
+        checkSum += c * w;
+    }
+
+    return checkSum % 10 == 0;
+}
+
