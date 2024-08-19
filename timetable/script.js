5d3ddd1910c2f211f387e2a7f442e768fd65b549
JelyFishhhhhh_ _ <tony0940419@gmail.com>
2024-08-19 20:45:35 +0800

    Commit

--- timetable/script.js
+++ timetable/script.js
@@ -2,7 +2,7 @@ window.addEventListener("load", function () {
     var table = document.querySelector("table");
     var container = document.querySelector(".calendar-container");
     var scale = Math.min(
-        container.clientWidth / table.offsetWidth,
+        (container.clientWidth - 100) / (table.offsetWidth - 100), // Subtract date column width
         container.clientHeight / table.offsetHeight
     );
     table.style.transform = "scale(" + scale + ")";
