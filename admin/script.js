6f9e0e7f25a8488f53b043a43d4c5af1eec43ec4
JelyFishhhhhh_ _ <tony0940419@gmail.com>
2024-08-23 00:45:40 +0800

    Commit

--- admin/script.js
+++ admin/script.js
@@ -154,6 +154,7 @@ async function getUserData() {
                 students.push(childSnapshot.val());
                 console.log(childSnapshot);
                 add2Table(
+                    count + 1,
                     Object.keys(snapshot.val())[count],
                     childSnapshot.val()
                 );
@@ -246,7 +247,7 @@ async function readUserData(stu) {
         });
 }
 
-function add2Table(sid, studentData) {
+function add2Table(index, sid, studentData) {
     const tableBody = document.getElementById("studentTableBody");
     const row = tableBody.insertRow();
 
